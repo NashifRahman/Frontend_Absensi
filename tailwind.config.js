1090e196a4a4f647bee28ba09c092f21e7c1f93b
NashifRahman <nashifrahman2004@gmail.com>
2024-12-12 11:20:32 +0700

    ini adalah frontend untuk aplikasi face recognition

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        customDarkBlue: "#0E142C",
+        hijau: "#3730a3",
+        unguHitam: "#250047",
+      },
+    },
+  },
+  plugins: [],
+};
+
